0d94fc8bf1916851ca28de58f15b74d25fb87b82
Admin <Admin@LAPTOP-VAF47LN9>
2023-04-09 17:47:26 +0530

    2

--- tc1/src/tc1/Accha.java
+++ tc1/src/tc1/Accha.java
@@ -3,7 +3,8 @@ package tc1;
 public class Accha {
 	public static void main(String[] args) {
 		System.out.println("kishan accha hai");
-		System.out.println('ek baar update ho gaya');
+		System.out.println("ek baar update ho gaya");
+		System.out.println("do baar update ho gaya" );
 		
 	}
 
